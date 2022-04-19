1f5589eaabd9fc0e7d193fac10ce3c2d3e3225f7
akilinjamam <akilinjamam@gmail.comm>
2022-04-20 01:15:21 +0600

    adding-files

---
+++ __steps.js
@@ -0,0 +1,18 @@
+/* 
+* 1 create a folder (mkdir or manually)
+* 2 open command line to that folder
+* 3 npm init
+* 4 npm install express
+* 4 create index.js
+* ---------------------
+* For Express / Node Server
+* ---------------------
+* 1 require express
+* 2 create app variable 
+* 3 declare app
+* 4 set app.get
+* 5 listen to port 
+* 6 node index.js
+* 7 check your browser for that port
+
+*/
